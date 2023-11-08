a2fbc93d0515fa2f790b66a61dd518408fa20a53
Josue Davila <josuedavila90@gmail.com>
2023-11-07 21:25:28 -0600

    Exercise 2 Added props to the Toast component and connected its values with the ToastPlayground for live-preview updates

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -3,12 +3,14 @@ import React from "react";
 import Button from "../Button";
 
 import styles from "./ToastPlayground.module.css";
+import Toast from "../Toast";
 
 const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
   const [message, setMessage] = React.useState("");
   const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+  const [openToast, setOpenToast] = React.useState(false);
 
   return (
     <div className={styles.wrapper}>
@@ -16,6 +18,10 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      <Toast open={openToast} onOpenChange={setOpenToast} variant={variant}>
+        {message}
+      </Toast>
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -63,11 +69,7 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button
-              onClick={() => alert(`Message: ${message} - Variant: ${variant}`)}
-            >
-              Pop Toast!
-            </Button>
+            <Button onClick={() => setOpenToast(true)}>Pop Toast!</Button>
           </div>
         </div>
       </div>
