a4ca7b214c51781ac13f68a869067364d9493675
Josue Davila <josuedavila90@gmail.com>
2023-11-14 13:23:13 -0600

    Exercise 5.1 on escape key down, dismiss all the toasts elements on screen.

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ToastShelf from "../ToastShelf";
 
-export const ToastContext = React.createContext(null);
+export const ToastContext = React.createContext({});
 function ToastProvider({ children }) {
   const [toasts, setToasts] = React.useState([]);
 
@@ -26,6 +26,19 @@ function ToastProvider({ children }) {
       },
     ]);
   }
+
+  useEffect(() => {
+    function onEscapeDown(e) {
+      console.log(e.key);
+      if (e.key === "Escape") {
+        setToasts([]);
+      }
+    }
+
+    window.addEventListener("keydown", onEscapeDown);
+
+    return () => window.removeEventListener("keydown", onEscapeDown);
+  }, []);
 
   return (
     <ToastContext.Provider value={{ showToast }}>
